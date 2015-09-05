50665fa2239bb7d40f757d7cadfbd8598b85f295
Lawrence Chan <chanlaw@seas.upenn.edu>
2015-09-05 03:27:16 -0400

    generated boilerplate with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pennAppsXiiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pennAppsXiiApp
+ */
+angular.module('pennAppsXiiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pennAppsXiiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pennAppsXiiApp
+ */
+angular.module('pennAppsXiiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
