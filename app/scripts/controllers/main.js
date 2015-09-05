06d5d17222e49fd015fdebb4f25be8c9957350a2
Lawrence Chan <chanlaw@seas.upenn.edu>
2015-09-05 11:10:08 -0400

    update index

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,21 @@
-'use strict';
+
 
 /**
  * @ngdoc function
- * @name pennAppsXiiApp.controller:MainCtrl
+ * @name pennAppsXiiApp.controller: MainCtrl
  * @description
  * # MainCtrl
  * Controller of the pennAppsXiiApp
  */
-angular.module('pennAppsXiiApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('app').controller("MainCtrl", function ($scope) {
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+
